08f7c22f959a9ff181188a5fc74299cf88a1a772
Anil Kumar <anil.kumar@prontoitlabs.com>
2017-07-28 02:36:07 +0530

    removed commented code

--- Helpers/AlertView+Helper.swift
+++ Helpers/AlertView+Helper.swift
@@ -9,8 +9,6 @@ import Gloss
 
 class Alert {
     
-//     let alertView = UIApplication.shared.windows[0].rootViewController
-    
     class func showAlertWithError(title: String ,message:String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
         alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: {(action: UIAlertAction!) in
--- Helpers/AppConstants.swift
+++ Helpers/AppConstants.swift
@@ -7,10 +7,5 @@
 //
 
 struct Constants {
-//    static let RESET_PASSWORD = "Reset Password"
-//    static let PLACEHOLDER_PASSWORD = "Enter.."
-//    static let RESET_PASSWORD_MESSAGE = "Enter Mobile No."
-//    static let RESET = "Reset"
-//    static let CANCEL = "Cancel"
     static let BASE_URL = "http://139.59.27.120:8080"
 }
